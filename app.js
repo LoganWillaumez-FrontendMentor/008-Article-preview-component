5d7e043b23c06ee303d6b8dcd2294132e2b8b6c5
Logan Willaumez <Logan.willaumez@gmail.com>
2021-10-14 13:00:13 +0200

    Finalising animation and javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector('.share-container');
+let card = document.querySelector('.card')
+let isClick = false;
+btn.addEventListener('click',clicker=>{
+    if(isClick==false){
+        card.classList.add('active');
+        isClick=true;
+    }
+    else{
+        card.classList.remove('active')
+        isClick=false;
+    }
+})
